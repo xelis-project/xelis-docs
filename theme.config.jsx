efb5a119bf5a4e6d581ac0dca51fc1f02faf1e0b
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2023-02-22 14:50:47 -0500

    head and footer customization

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,27 @@
+import { useConfig } from 'nextra-theme-docs'
+
 export default {
   logo: <span>Xelis Blockchain</span>,
   project: {
     link: 'https://github.com/xelis-project'
   },
-  docsRepositoryBase: 'https://github.com/xelis-project/xelis-docs/blob/master'
+  docsRepositoryBase: 'https://github.com/xelis-project/xelis-docs/blob/master',
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – Xelis Docs'
+    }
+  },
+  head: () => {
+    const { frontMatter } = useConfig()
+    return <>
+      <meta property="og:title" content={frontMatter.title || 'Xelis Docs'} />
+      <meta property="og:description" content={frontMatter.description || 'Detailed information on the core features and functionality of the Xelis Blockchain'} />
+      {frontMatter.image && <meta name="og:image" content={frontMatter.image} />}
+    </>
+  },
+  footer: {
+    text: <span>
+      {new Date().getFullYear()} © <a href="https://xelis.io" target="_blank">Xelis Blockchain</a>
+    </span>
+  }
 }
