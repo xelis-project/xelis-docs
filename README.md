4d5f3fa9586df7ae3c9b2ce2400819f7809ce653
Slixe <slixeprivate@gmail.com>
2023-07-01 15:06:39 +0200

    add favicon.io, set XELIS to uppercase

---
+++ README.md
@@ -0,0 +1,6 @@
+# XELIS Docs
+
+This repository contains all documentation available about XELIS and is available online at https://docs.xelis.io
+
+## Getting Started
+
--- theme.config.jsx
+++ theme.config.jsx
@@ -1,27 +1,28 @@
 import { useConfig } from 'nextra-theme-docs'
 
 export default {
-  logo: <span>Xelis Blockchain</span>,
+  logo: <span>XELIS Blockchain</span>,
   project: {
     link: 'https://github.com/xelis-project'
   },
   docsRepositoryBase: 'https://github.com/xelis-project/xelis-docs/blob/master',
   useNextSeoProps() {
     return {
-      titleTemplate: '%s – Xelis Docs'
+      titleTemplate: '%s – XELIS Docs'
     }
   },
   head: () => {
     const { frontMatter } = useConfig()
     return <>
-      <meta property="og:title" content={frontMatter.title || 'Xelis Docs'} />
-      <meta property="og:description" content={frontMatter.description || 'Detailed information on the core features and functionality of the Xelis Blockchain'} />
+      <meta property="og:title" content={frontMatter.title || 'XELIS Docs'} />
+      <meta property="og:description" content={frontMatter.description || 'Detailed information on the core features and functionality of the XELIS Blockchain'} />
       {frontMatter.image && <meta name="og:image" content={frontMatter.image} />}
+      <link rel="icon" href="/favicon.ico" sizes="any" />
     </>
   },
   footer: {
     text: <span>
-      {new Date().getFullYear()} © <a href="https://xelis.io" target="_blank">Xelis Blockchain</a>
+      {new Date().getFullYear()} © <a href="https://xelis.io" target="_blank">XELIS Blockchain</a>
     </span>
   }
 }
