1c18cc9d065ac6f90060e564aa66f4181db53bf3
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2025-09-23 12:50:37 -0400

    remove banner

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,5 @@
 import { Footer, Layout, Navbar } from 'nextra-theme-docs'
-import { Banner, Head } from 'nextra/components'
+import { Head } from 'nextra/components'
 import { getPageMap } from 'nextra/page-map'
 import 'nextra-theme-docs/style.css'
 
@@ -8,8 +8,6 @@ export const metadata = {
   // For more information on metadata API, see: https://nextjs.org/docs/app/building-your-application/optimizing/metadata
 }
  
-const banner = <Banner storageKey="some-key">Nextra 4.0 is released 🎉</Banner>
-
 const navbar = (
   <Navbar
     projectLink="https://github.com/xelis-project"
@@ -33,7 +31,6 @@ export default async function RootLayout({ children }) {
       </Head>
       <body>
         <Layout
-          banner={banner}
           navbar={navbar}
           pageMap={await getPageMap()}
           docsRepositoryBase="https://github.com/xelis-project/xelis-docs/blob/master"
