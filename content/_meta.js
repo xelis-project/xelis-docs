06491b974f9c9b02af0929a6b022ad1c5601491e
Slixe <slixeprivate@gmail.com>
2026-04-29 22:23:49 +0200

    remove duplicated links

--- content/_meta.js
+++ content/_meta.js
@@ -26,30 +26,5 @@ export default {
   "development-status": "Development Status",
   "tokenomics": "Tokenomics",
   "network-upgrades": "Network Upgrades",
-  "faq": "FAQ",
-  "xelis": {
-    "title": "Website",
-    "href": "https://xelis.io",
-    "type": "page"
-  },
-  "whitepaper": {
-    "title": "Whitepaper",
-    "href": "https://whitepaper.xelis.io",
-    "type": "page"
-  },
-  "wallet": {
-    "title": "Web Wallet",
-    "href": "https://wallet.xelis.io",
-    "type": "page"
-  },
-  "explorer": {
-    "title": "Explorer",
-    "href": "https://explorer.xelis.io",
-    "type": "page"
-  },
-  "stats": {
-    "title": "Statistics",
-    "href": "https://stats.xelis.io",
-    "type": "page"
-  }
+  "faq": "FAQ"
 }
